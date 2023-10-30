1f26a3416e495e41dc50526b71393efe437c4fb3
jamie <happy_programming@protonmail.com>
2023-10-30 23:56:48 +1300

    added specialties map impl

---
+++ pet-clinic-data/src/main/java/spencer/springframework/petclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package spencer.springframework.petclinic.services;
+
+import spencer.springframework.petclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/spencer/springframework/petclinic/services/map/SpecialitiesServiceMap.java
@@ -0,0 +1,35 @@
+package spencer.springframework.petclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import spencer.springframework.petclinic.model.Speciality;
+import spencer.springframework.petclinic.services.SpecialitiesService;
+
+import java.util.Set;
+
+@Service
+public class SpecialitiesServiceMap extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
