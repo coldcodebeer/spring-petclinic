7c1d9b9144bc2dcba5935859e6d6f3efcd156d48
jamie <happy_programming@protonmail.com>
2023-10-30 18:46:57 +1300

    added PetTypeService closes #8

---
+++ pet-clinic-data/src/main/java/spencer/springframework/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package spencer.springframework.petclinic.services;
+
+import spencer.springframework.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
